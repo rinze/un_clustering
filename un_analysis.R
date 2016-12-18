ca38b0132203ec87d9cba962013601a19053428a
José María Mateos <chema@rinzewind.org>
2016-12-18 17:49:54 -0500

    Good tsne parameters (initial_dims = 50, perplexity = 20, whiten = FALSE, with cosine metric

--- un_analysis.R
+++ un_analysis.R
@@ -16,20 +16,28 @@ cc_data <- cc_data[!duplicated(cc_data$ccode), ]
 
 # Get raw voting data and obtain year and country name
 raw_data$year <- 1945 + raw_data$session
+# Remove non-members (vote code 9)
+raw_data <- raw_data[which(raw_data$vote != 9), ]
 raw_data <- left_join(raw_data, cc_data, by = "ccode")
 # Let's suppose I can get rid of country 511 (don't have name) and NA.
-# Can't find them in the codebook (?)
+# Can't find it in the codebook (?)
 raw_data <- raw_data[!is.na(raw_data$CountryName), ]
+
+# Very simple check. Should be similar to this table:
+# http://www.un.org/en/sections/member-states/growth-united-nations-membership-1945-present/index.html
+l <- raw_data %>% group_by(year) %>% summarise(n = length(unique(CountryName)))
+plot(l$year, l$n)
 
 # t-SNE for clustering!
 # Color: distance to USA / URSS until 1991. Later: ?
 
 # Test
 raw_data_1946 <- raw_data[raw_data$year == 1946, ]
+raw_data_1946$CountryName <- factor(raw_data_1946$CountryName)
 # Or:
-# raw_data_1946_wide <- dcast(raw_data_1946, CountryName ~ rcid + vote)
+# raw_data_1946_wide <- dcast(raw_data_1946, CountryName ~ rcid + vote, fun.aggregate = length)
 # And use that directly (save for the first column)
-raw_data_1946_wide <- dcast(raw_data_1946, rcid + vote ~ CountryName)
+raw_data_1946_wide <- dcast(raw_data_1946, rcid + vote ~ CountryName, fun.aggregate = length)
 cos <- cosine(as.matrix(raw_data_1946_wide[, -c(1, 2)]))
 t_1946 <- tsne(cos, k = 2, initial_dims = 30,
                perplexity = 30, whiten = FALSE)
